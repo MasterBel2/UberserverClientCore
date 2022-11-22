89ecfafecc1268d424ec528d457d473da1ec04d8
MasterBel2 <untakenprefix@gmail.com>
2022-11-22 15:36:53 +1000

    `System`: add Linux implementation, return `Process` on app launch
    Returning process allows for management of an app instance - e.g. termination

--- Sources/UberserverClientCore/Utility/Platform/System.swift
+++ Sources/UberserverClientCore/Utility/Platform/System.swift
@@ -22,9 +22,9 @@ public protocol System: AnyObject {
     - parameter arguments: Arguments passed to the application on launch.
     - parameter completionHandler: called after the termination of the application.
     */
-    func launchApplication(at path: String, with arguments: [String]?, completionHandler: (() -> Void)?)
+    func launchApplication(at path: String, with arguments: [String]?, completionHandler: (() -> Void)?) -> Process
     /// Searches for and launches an application.
-    func launchApplication(_ applicationName: String, with arguments: [String]?, completionHandler: (() -> Void)?)
+    func launchApplication(_ applicationName: String, with arguments: [String]?, completionHandler: (() -> Void)?) -> Process
 
     /// Spring's data directory.
     var dataDirectory: URL { get }
@@ -62,16 +62,16 @@ public final class MacOS: System {
         NSWorkspace.shared.selectFile(fullDirectory?.path, inFileViewerRootedAtPath: directory.path)
     }
 
-    public func launchApplication(_ application: String, with arguments: [String]?, completionHandler: (() -> Void)?) {
+    public func launchApplication(_ application: String, with arguments: [String]?, completionHandler: (() -> Void)?) -> Process {
         guard let path = NSWorkspace.shared.fullPath(forApplication: application) else {
             debugPrint("Non-Fatal Error: could not find \(application)")
             completionHandler?()
             return
         }
-        launchApplication(at: path, with: arguments, completionHandler: completionHandler)
+        return launchApplication(at: path, with: arguments, completionHandler: completionHandler)
     }
 
-    public func launchApplication(at path: String, with arguments: [String]?, completionHandler: (() -> Void)?) {
+    public func launchApplication(at path: String, with arguments: [String]?, completionHandler: (() -> Void)?) -> Process {
         guard let bundle = Bundle(path: path) else {
             debugPrint("Non-Fatal Error: could not create bundle object at \(path)")
             return
@@ -86,10 +86,61 @@ public final class MacOS: System {
         }
         process.launch()
         processes.append(process)
+
+        return process
     }
 
     public func write(_ fileContents: Data, to directory: URL) {
         fileManager.createFile(atPath: directory.path, contents: fileContents, attributes: nil)
     }
 }
-#endif // macOS
+
+#elseif os(Linux)
+
+public class Linux: System {
+    public init() {}
+
+    var processes: [Process] = []
+
+    public func write(_ fileContents: Data, to directory: URL) {
+        try! fileContents.write(to: directory)
+    }
+
+    public func showFile(_ fileName: String?, at directory: URL) {
+
+    }
+
+    public func launchApplication(at path: String, with arguments: [String]?, completionHandler: (() -> Void)?) -> Process {
+        let process = Process()
+        process.launchPath = path
+        process.arguments = arguments
+        process.terminationHandler = { [weak self] process in
+            self?.processes.removeAll(where: {$0 === process})
+            completionHandler?()
+        }
+
+        do {
+            try process.run()
+        } catch {
+            print("Failed to run application at \(path): \(error)")
+        }
+        
+        processes.append(process)
+        return process
+    }
+
+    public func launchApplication(_ applicationName: String, with arguments: [String]?, completionHandler: (() -> Void)?) -> Process {
+        fatalError()
+    }
+    public var dataDirectory: URL { 
+        // return NSHomeDirectoryURL().appendingPathComponent(".spring", isDirectory: true)
+        return configDirectory
+    }
+    public var configDirectory: URL { 
+        // return NSHomeDirectoryURL().appendingPathComponent(".config", isDirectory: true).appendingPathComponent("spring", isDirectory: true)
+
+        return NSHomeDirectoryURL().appendingPathComponent("Documents", isDirectory: true).appendingPathComponent("Beyond All Reason", isDirectory: true)
+    }
+}
+
+#endif
